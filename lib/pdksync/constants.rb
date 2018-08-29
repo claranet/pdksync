1bf463a12056015cdcad95b2e04ddc1f6c74bc4e
Craig Watson <craig.watson@claranet.uk>
2018-08-29 11:29:19 +0100

    Making Claranet-specific changes

--- lib/pdksync/constants.rb
+++ lib/pdksync/constants.rb
@@ -3,7 +3,7 @@
 module PdkSync # rubocop:disable Style/ClassAndModuleChildren
   module Constants
     ACCESS_TOKEN = ENV['GITHUB_TOKEN'].freeze
-    NAMESPACE = 'puppetlabs'.freeze
+    NAMESPACE = 'claranet'.freeze
     PDKSYNC_DIR = 'modules_pdksync'.freeze
     PUSH_FILE_DESTINATION = 'origin'.freeze
     CREATE_PR_AGAINST = 'master'.freeze
